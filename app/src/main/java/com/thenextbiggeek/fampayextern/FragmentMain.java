5716fcd870510ad22a3a7be1d5822801ba166f97
abilashsenth <abilashsenth@gmail.com>
2021-12-18 14:54:08 +0530

    fragment_main.xml and FragmentMain.java added

---
+++ app/src/main/java/com/thenextbiggeek/fampayextern/FragmentMain.java
@@ -0,0 +1,62 @@
+package com.thenextbiggeek.fampayextern;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thenextbiggeek.fampayextern.databinding.FragmentMainBinding;
+
+public class FragmentMain extends Fragment {
+    private FragmentMainBinding binding;
+
+    public FragmentMain() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        isNetworkAvailable();
+    }
+
+    private void isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) requireActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean netStatus = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        if (!netStatus) {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+            builder1.setMessage("Looks like you're offline 👀...");
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton(
+                    "OK",
+                    (dialog, id) -> dialog.cancel());
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+    }
+}
--- app/src/main/java/com/thenextbiggeek/fampayextern/MainActivity.java
+++ app/src/main/java/com/thenextbiggeek/fampayextern/MainActivity.java
@@ -1,14 +1,49 @@
 package com.thenextbiggeek.fampayextern;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.thenextbiggeek.fampayextern.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View rootView = binding.getRoot();
+        setContentView(rootView);
+        setFullscreenMode(getWindow().getDecorView(), getWindow());
+        loadFragment(new FragmentMain(), savedInstanceState);
+
+
+    }
+
+    private void loadFragment(Fragment fragment, Bundle savedInstanceState) {
+        if (fragment != null && savedInstanceState !=null) {
+            getSupportFragmentManager().beginTransaction().
+                    setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, FragmentMain.class, null)
+                    .commit();
+        }
+    }
+
+    public void setFullscreenMode(View decorView, Window w) {
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //w.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+
+
     }
 }
